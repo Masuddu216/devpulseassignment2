54d7a7c3e970555eb7319c59baad303ed652407e
Masud <masuddu101@gmail.com>
2026-06-13 17:45:58 +0600

    Update project

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,14 +4,14 @@ import { sendSuccess } from '../../utils/response.util.js';
 import { validateLoginBody, validateSignupBody } from './auth.validation.js';
 import { loginUser, registerUser } from './auth.service.js';
 
-// export async function signup(req: Request, res: Response): Promise<void> {
-//   const validatedBody = validateSignupBody(req.body);
-//   const user = await registerUser(validatedBody);
-//   sendSuccess(res, StatusCodes.CREATED, 'User registered successfully', user);
-// }
+export async function signup(req: Request, res: Response): Promise<void> {
+  const validatedBody = validateSignupBody(req.body);
+  const user = await registerUser(validatedBody);
+  sendSuccess(res, StatusCodes.CREATED, 'User registered successfully', user);
+}
 
-// export async function login(req: Request, res: Response): Promise<void> {
-//   const validatedBody = validateLoginBody(req.body);
-//   const result = await loginUser(validatedBody);
-//   sendSuccess(res, StatusCodes.OK, 'Login successful', result);
-// }
+export async function login(req: Request, res: Response): Promise<void> {
+  const validatedBody = validateLoginBody(req.body);
+  const result = await loginUser(validatedBody);
+  sendSuccess(res, StatusCodes.OK, 'Login successful', result);
+}
